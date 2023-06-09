3758ebe6ab758682926af1fbd5bdec15f120a2c4
bsc-com-33-20 <bsc-com-33-20@unima.ac.mw>
2023-06-09 16:49:57 +0200

    changed the import for forFeature in booking module

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
-import { Booking } from 'src/typeorm/Booking';
+import { Booking } from '../typeorm/Booking';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookingInspections } from 'src/typeorm/Inspections';
 
