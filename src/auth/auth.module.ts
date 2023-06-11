fe98610244af57eccefd07858f558587c71d5862
bsc-com-33-20 <bsc-com-33-20@unima.ac.mw>
2023-06-12 00:15:20 +0200

    auth module commentd provider

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { MagicLoginStrategy } from './magiclogin.strategy';
 import { UsersService } from 'src/users/users.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, /*MagicLoginStrategy*/, UsersService]
+  providers: [AuthService,UsersService]
 })
 export class AuthModule {}
